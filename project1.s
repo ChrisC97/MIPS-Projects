5bd13eb9d8cf6ce928b6271093ba779ef1cc5dc0
ChrisC97 <christopher.collins@bison.howard.edu>
2019-10-22 16:25:01 -0400

    Getting user input, Comments.

--- project1.s
+++ project1.s
@@ -1,13 +1,21 @@
 .data
 	base: .word 23
 	sr1: .asciiz "Enter 10 characters: "
+	userNumber: .space 11
 
 .text # Instructions section, goes in text segment.
 
 main:
+	# PRINT SR1 #
     li $v0, 4 # System call to print a string.
 	la $a0, sr1 # Load string to be printed.
 	syscall # Print string.
+	
+	# READ USER INPUT #
+	li $v0, 8 # System call for taking in input.
+	la $a0, userNumber # Where the string is saved.
+	li $a1, 11 # Max number of characters to read.
+	syscall
 	
 	li $v0, 10 # Exit program system call.
 	syscall
