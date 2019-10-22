5124f223f7f9ca8932cc4dcc1feb3d2b6d9f7364
ChrisC97 <christopher.collins@bison.howard.edu>
2019-10-22 16:12:56 -0400

    Fixed comments.

--- project1.s
+++ project1.s
@@ -2,12 +2,12 @@
 	base: .word 23
 	sr1: .asciiz "Enter 10 characters: "
 
-.text ## Instructions section, goes in text segment.
+.text # Instructions section, goes in text segment.
 
 main:
-    li $v0, 4 ## System call to print a string.
-	la $a0, sr1 ## Load string to be printed.
-	syscall ## Print string.
+    li $v0, 4 # System call to print a string.
+	la $a0, sr1 # Load string to be printed.
+	syscall # Print string.
 	
-	li $v0, 10 ## Exit program system call.
+	li $v0, 10 # Exit program system call.
 	syscall
